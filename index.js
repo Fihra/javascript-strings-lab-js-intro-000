f172e470f7e0538fb6c1082946c23c982e25f5f3
Fabian Fabro <fabrofabian@gmail.com>
2018-10-07 11:09:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ var greetSpecialGuest = greeting2 + specialGuest + "!";
 
 var topic = "space";
 
-var topic2 = "Let\s talk about";
+var topic2 = "Let\'s talk about";
 
 var conversation = `${topic2 + " " + topic + "."}`;
